f9ecb1a512cbeef09da106c595df14c0d741d42e
bhavan-3 <chimmilibhavani241@gmail.com>
2026-02-14 09:53:48 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,11 @@
+import random
+import string
+length = int(input('enter the length of password ='))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + symbols
+temp = random.sample(all,length)
+password = "".join(temp)
+print(password)
